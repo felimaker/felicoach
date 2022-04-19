5902e491e19ececbad47d553db244318fe2cc293
felimaker <felicoach@gmail.com>
2022-04-19 17:53:25 -0500

    Mas 2

--- mail.php
+++ mail.php
@@ -1,9 +1,9 @@
 <?php
 	$to = "felicoach@gmail.com";
-	$tema = "Contacto web Felicoach.com";
-	$message = "Name: ".$_POST['name']."<br>";
+	$tema = "Form de web Felicoach";
+	$message = "Nombre: ".$_POST['name']."<br>";
 	$message .= "Email: ".$_POST['email']."<br>";
-	$message .= "Message: ".$_POST['note']."<br>";
+	$message .= "Mensaje: ".$_POST['note']."<br>";
 	$headers  = 'MIME-Version: 1.0' . "\r\n";
 		$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
 	mail($to, $tema, $message, $headers);
